91071b89c0a5321307880205cff6b335bf084946
Falcohub <57810550+Falcohub@users.noreply.github.com>
2019-11-18 15:17:40 -0500

    Add files via upload

---
+++ ProyectoElectronica/ProyectoElectronica/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ProyectoElectronica
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.Btn_Sala2 = new System.Windows.Forms.Button();
+			this.Btn_Fachada = new System.Windows.Forms.Button();
+			this.Btn_Sala1 = new System.Windows.Forms.Button();
+			this.Btn_Cocina = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(47, 74);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Sala";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(47, 354);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 20);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Sala";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(351, 18);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(78, 25);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Piso 2";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(351, 204);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(78, 25);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Piso 1";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(526, 354);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(58, 20);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Cocina";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(526, 74);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(72, 20);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Fachada";
+			// 
+			// Btn_Sala2
+			// 
+			this.Btn_Sala2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_Sala2.BackgroundImage")));
+			this.Btn_Sala2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.Btn_Sala2.ForeColor = System.Drawing.SystemColors.MenuText;
+			this.Btn_Sala2.Location = new System.Drawing.Point(121, 36);
+			this.Btn_Sala2.Name = "Btn_Sala2";
+			this.Btn_Sala2.Size = new System.Drawing.Size(125, 99);
+			this.Btn_Sala2.TabIndex = 6;
+			this.Btn_Sala2.UseVisualStyleBackColor = true;
+			this.Btn_Sala2.Click += new System.EventHandler(this.Btn_Sala2_Click);
+			// 
+			// Btn_Fachada
+			// 
+			this.Btn_Fachada.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_Fachada.BackgroundImage")));
+			this.Btn_Fachada.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.Btn_Fachada.Location = new System.Drawing.Point(635, 36);
+			this.Btn_Fachada.Name = "Btn_Fachada";
+			this.Btn_Fachada.Size = new System.Drawing.Size(125, 99);
+			this.Btn_Fachada.TabIndex = 7;
+			this.Btn_Fachada.UseVisualStyleBackColor = true;
+			this.Btn_Fachada.Click += new System.EventHandler(this.Btn_Fachada_Click);
+			// 
+			// Btn_Sala1
+			// 
+			this.Btn_Sala1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_Sala1.BackgroundImage")));
+			this.Btn_Sala1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.Btn_Sala1.Location = new System.Drawing.Point(121, 316);
+			this.Btn_Sala1.Name = "Btn_Sala1";
+			this.Btn_Sala1.Size = new System.Drawing.Size(125, 99);
+			this.Btn_Sala1.TabIndex = 8;
+			this.Btn_Sala1.UseVisualStyleBackColor = true;
+			this.Btn_Sala1.Click += new System.EventHandler(this.Btn_Sala1_Click);
+			// 
+			// Btn_Cocina
+			// 
+			this.Btn_Cocina.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Btn_Cocina.BackgroundImage")));
+			this.Btn_Cocina.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.Btn_Cocina.Location = new System.Drawing.Point(635, 316);
+			this.Btn_Cocina.Name = "Btn_Cocina";
+			this.Btn_Cocina.Size = new System.Drawing.Size(125, 99);
+			this.Btn_Cocina.TabIndex = 9;
+			this.Btn_Cocina.UseVisualStyleBackColor = true;
+			this.Btn_Cocina.Click += new System.EventHandler(this.Btn_Cocina_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Btn_Cocina);
+			this.Controls.Add(this.Btn_Sala1);
+			this.Controls.Add(this.Btn_Fachada);
+			this.Controls.Add(this.Btn_Sala2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button Btn_Sala2;
+		private System.Windows.Forms.Button Btn_Fachada;
+		private System.Windows.Forms.Button Btn_Sala1;
+		private System.Windows.Forms.Button Btn_Cocina;
+	}
+}
+
---
+++ ProyectoElectronica/ProyectoElectronica/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoElectronica
+{
+	public partial class Form1 : Form
+	{
+		private int sala1 = 0;
+		private int cocina = 0;
+		private int sala2 = 0;
+		private int fachada = 0;
+
+		System.IO.Ports.SerialPort arduino;
+		public Form1()
+		{
+			InitializeComponent();
+			arduino = new System.IO.Ports.SerialPort();
+			arduino.PortName = "COM4";
+			arduino.BaudRate = 9600;
+			arduino.Open();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (arduino.IsOpen) arduino.Close();
+		}
+
+		private void Btn_Sala1_Click(object sender, EventArgs e)
+		{
+			if(sala1 == 0)
+			{
+				arduino.Write("a");
+				sala1 = 1;
+			}
+			else
+			{
+				arduino.Write("b");
+				sala1 = 0;
+			}
+		}
+
+		private void Btn_Cocina_Click(object sender, EventArgs e)
+		{
+			if (cocina == 0)
+			{
+				arduino.Write("c");
+				cocina = 1;
+			}
+			else
+			{
+				arduino.Write("d");
+				cocina = 0;
+			}
+		}
+
+		private void Btn_Sala2_Click(object sender, EventArgs e)
+		{
+			if (sala2 == 0)
+			{
+				arduino.Write("e");
+				sala2 = 1;
+			}
+			else
+			{
+				arduino.Write("f");
+				sala2 = 0;
+			}
+		}
+
+		private void Btn_Fachada_Click(object sender, EventArgs e)
+		{
+			if (fachada == 0)
+			{
+				arduino.Write("g");
+				fachada = 1;
+			}
+			else
+			{
+				arduino.Write("h");
+				fachada = 0;
+			}
+		}
+	}
+}
